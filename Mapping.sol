a44f34be9ead8818a257bb9d0dedfcb86a46fd93
OmkarK97 <omkar139797@gmail.com>
2023-04-05 01:45:46 +0530

    Mapping

--- Mapping.sol
+++ Mapping.sol
@@ -8,13 +8,7 @@ contract Mapping {
 
     function examples() external {
         balances[msg.sender] = 123;
-        uint bal = balances[msg.sender];
-        uint bal2 = balances[address(1)];
-
-        balances[msg.sender] += 456;
-
         delete balances[msg.sender];
-
         isFriend[msg.sender][address(this)] = true;
     }
 
